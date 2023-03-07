cdab7b2b4c9f556467efad94336810df3beec754
Joseph Ampadu <josephampadu549@gmail.com>
2023-03-07 16:13:46 +0000

    add file storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,8 @@
 import uuid
 from datetime import datetime
 
+from models import storage
+
 
 class BaseModel:
     """Defines the base model"""
@@ -20,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """String representation of the base model instance"""
@@ -30,6 +33,7 @@ class BaseModel:
     def save(self):
         """Saves the base model instance"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
