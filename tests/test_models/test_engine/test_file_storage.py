5e10eed9b6d451d250ea2ab476a2b8e480f0cdd7
Joseph Ampadu <josephampadu549@gmail.com>
2023-03-12 16:14:00 +0000

    check type of FileStorage.all return value

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -26,6 +26,7 @@ class TestFileStorage(unittest.TestCase):
         """Test all public instance method"""
         fstorage = FileStorage()
         result = fstorage.all()
+        self.assertIsInstance(result, dict)
         self.assertDictEqual(result, FileStorage._FileStorage__objects)
 
     def test_new(self):
